f1b14c1dcf6898a0c5d25973fd9cacf2f704ea3c
Shams Sannoufa <shamssannoufa@gmail.com>
2025-07-09 17:24:40 +0300

    firsm commit

---
+++ src/Counter.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './counter.scss'; 
+
+
+class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      count:0
+    }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  increment(){
+    this.setState(state => ({
+      count : state.count +1
+    }));
+    }
+  decrement(){
+    this.setState(state => ({
+      count : state.count -1
+    }));
+  }
+
+  reset(){
+    this.setState({
+      count:0
+    });
+
+  }
+  render(){
+    return(
+      <div className='counter-container' style={{textAlign: 'center', marginTop: '50px'}}>
+        <h1>Counter: {this.state.count} </h1>
+
+        <button className='btn inc' onClick={this.increment}>increment</button>
+        <button className='btn dec' onClick={this.reset}>reset</button>
+        <button className='btn reset' onClick={this.decrement}>decrement</button>
+      </div>
+    )
+  }
+
+
+}
+
+export default Counter;
